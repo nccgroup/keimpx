ab038314db311001cf282d48caa73e50b7ce412d
Bernardo Damele <bernardo.damele@gmail.com>
2013-12-24 12:19:17 +0100

    temporary patch for Impacket version logger

--- lib/logger.py
+++ lib/logger.py
@@ -5,7 +5,16 @@
 import logging
 import sys
 
-logger = logging.getLogger('logger')
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
+logging.addLevelName(logging.ERROR, 'ERROR')
+logging.addLevelName(logging.WARNING, 'WARNING')
+logging.addLevelName(logging.CRITICAL, 'CRITICAL')
+logging.addLevelName(logging.INFO, 'INFO')
+logging.addLevelName(logging.DEBUG, 'DEBUG')
+
+logger = logging.getLogger()
 logger_handler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', '%H:%M:%S')
 logger_handler.setFormatter(formatter)
