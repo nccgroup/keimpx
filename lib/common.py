9265c1e6b1aa2f7cfcf35acfbb77ba235bebd1b7
Bernardo Damele <bernardo.damele@gmail.com>
2014-03-04 14:23:39 +0000

    verbose error message if pycrypto is not installed

--- lib/common.py
+++ lib/common.py
@@ -81,7 +81,7 @@ try:
     from Crypto.Cipher import DES, ARC4, AES
     from Crypto.Hash import HMAC, MD4
 except ImportError:
-    sys.stderr.write('You do not have any crypto installed. You need PyCrypto.\nGet it from http://www.pycrypto.org')
+    sys.stderr.write('You do not have any crypto installed. You need PyCrypto.\nRun: apt-get install python-crypto or get it from http://www.pycrypto.org')
     sys.exit(255)
 
 from lib.exceptions import *
