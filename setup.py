5d1e0149418a3b7f63e8a2b2b2f13a241e3771f1
Joshua Gregory <NEXUS2345@users.noreply.github.com>
2019-11-29 00:31:20 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ from distutils.core import setup
 
 setup(
     name="keimpx",
-    version="0.4",
+    version="0.5-dev",
     description="keimpx: check for the usefulness of credentials across a network over SMB",
     author="Bernardo Damele A. G.",
     author_email="bernardo.damele@gmail.com",
